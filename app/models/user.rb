f4073cf688fc9de6a6455067982204a09ca7b804
masato315 <masato.kino315@gmail.com>
2022-03-26 15:11:17 +0900

    userモデル　バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password         , format: { with: VALID_PASSWORD_REGEX}
   validates :name             ,presence: true
-  validates :first_name       ,presence: true
-  validates :last_name        ,presence: true
-  validates :kana_first_name  ,presence: true
-  validates :kana_last_name   ,presence: true
+  validates :first_name       ,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name        ,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :kana_first_name  ,presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :kana_last_name   ,presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday         ,presence: true
 end
