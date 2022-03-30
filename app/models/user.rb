05cef86fe2cf396aaee4ec257fade70579e0459c
masato315 <masato.kino315@gmail.com>
2022-03-30 23:52:10 +0900

    商品出品機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+
+
+  has_many :items
 end
