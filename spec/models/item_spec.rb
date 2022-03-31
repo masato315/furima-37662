b64f37654f93ce13aec6e64d4a14e56ae35a4739
masato315 <masato.kino315@gmail.com>
2022-03-31 16:47:36 +0900

    itemモデル単体テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '出品できる場合'  do
+      it '全て正しく入力されてると出品できる'do
+       expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it '商品画像が空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では出品できない' do
+        @item.goods_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Goods name can't be blank")
+      end
+      it '商品の説明が空では出品できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'カテゴリーが空では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態が空では出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担が空では出品できない' do
+        @item.delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank")
+      end
+      it '発送元の地域が空では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が空では出品できない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '販売価格が空ではできない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number", "Price is invalid")
+      end
+      it '販売価格が半角数値以外だと出品できない' do
+        @item.price = "a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
